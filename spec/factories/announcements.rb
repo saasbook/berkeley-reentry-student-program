f68d8a159efe9e098ffdc5b36266f5d3f0b9e399
KLongLo <kinlonglo98065@gmail.com>
2022-03-05 19:07:03 -0800

    make better factory for announcement

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     Time.zone = 'Pacific Time (US & Canada)'
     sequence(:title) { |n| "Announcement #{n} Title" }
     sequence(:content) { |n| "Announcement #{n} content" }
-    issued_date { Date.parse(Time.now.to_s) }
+    issued_date { DateTime.now.to_date }
     association :admin, factory: :admin
   end
 end
