929158a1a43a597e1a3e0cb2ab426794ccde9645
KLongLo <kinlonglo98065@gmail.com>
2022-03-07 16:03:17 -0800

    test: add rspec test for student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+include FactoryBot::Syntax::Methods
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "checks if student have sid before saving" do
+    n_student = build :student
+    n_student.sid = nil
+    expect{n_student.save!}.to raise_error
+    
+  end
+
+  it "doesn't allows non student user to be saved as student" do
+    n_student = build :student
+    n_student.is_student = false
+    expect{n_student.save!}.to raise_error(Exception, "This user must be a student!!")
+  end
+
+  it 'checks-in' do
+    student = create :student
+    student.checkin
+    expect(!!(CheckinRecord.find_by student: student)).to be true
+  end
 end
