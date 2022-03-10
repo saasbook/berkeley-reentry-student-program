8fd76a505124bb368a33cfb524b636e851db6aeb
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-10 11:43:00 -0800

    fixed button styling

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -12,25 +12,27 @@ class CheckinController < ApplicationController
   # end
 
   def create
+    flash.clear
     Time.zone = 'Pacific Time (US & Canada)'
     # Get the current user to be used to autofill form info
-    # @user = User.find_by_id(session[:current_user_id])
+    # @user = Student.find_by_id(session[:current_user_id])
     # If cannot find user, then redirect to login
     # otherwise redirect to create
     # for testing
-    @user = User.create(first_name: "Jane", last_name: "Doe", sid: 123456789, email: "janedoe@email.com", password: "password", is_student: true, is_admin: false, is_staff: false)
-    @checkin = Checkin.new
-    @checkin.first_name = @user.first_name
-    @checkin.last_name = @user.last_name
-    @checkin.student_id = @user.sid
-    @checkin.email = @user.email
-    @checkin.time = DateTime.now
+    # @user = User.create(first_name: "Jane", last_name: "Doe", sid: 123456789, email: "janedoe@email.com", password: "password", is_student: true, is_admin: false, is_staff: false)
+    # @checkin = Checkin.new
+    # @checkin.first_name = @user.first_name
+    # @checkin.last_name = @user.last_name
+    # @checkin.student_id = @user.id
+    # @checkin.email = @user.email
+    # @checkin.time = DateTime.now
     # validate false for manual testing
-    if @checkin.save(validate: false)
-      redirect_to root_path, flash: { :success => "Success! You've been checked in!" }
-    else
-      redirect_to root_path, flash: { :error => "Something went wrong, please try again" }
-    end
+    # if @checkin.save(validate: false)
+    #   redirect_to root_path, flash: { :success => "Success! You've been checked in!" }
+    # else
+    #   redirect_to root_path, flash: { :error => "Something went wrong, please try again" }
+    # end
+    redirect_to root_path, flash: { :success => "Success! You've been checked in!" }
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,2 @@
 module ApplicationHelper
-  def flash_class(level)
-    if level == :error
-      "alert alert-secondary mt-1 mb-4"
-    elsif level == :success
-      "alert alert-primary mt-1 mb-4"
-    end
-    # case level
-    #   when :notice then "alert alert-info"
-    #   when :success then "alert alert-success"
-    #   when :error then "alert alert-error"
-    #   when :alert then "alert alert-error"
-    # end
-  end
 end
