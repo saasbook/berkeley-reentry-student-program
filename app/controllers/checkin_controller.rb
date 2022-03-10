a928dbe32f3dd1502897bc633bac2e3c48c152d6
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-09 18:26:58 -0800

    update

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -20,7 +20,7 @@ class CheckinController < ApplicationController
     # for testing
     @user = User.create(first_name: "Jane", last_name: "Doe", sid: 123456789, email: "janedoe@email.com", password: "password", is_student: true, is_admin: false, is_staff: false)
     @checkin = Checkin.new
-    @checkin.update(time: DateTime.now, student_id: @user.student_id, email: @user.email, first_name: @user.first_name, last_name: @user.last_name)
+    @checkin.update(time: DateTime.now, student_id: @user.sid, email: @user.email, first_name: @user.first_name, last_name: @user.last_name)
     if @checkin.save(validate: false)
       redirect_to root_path, flash: { :success => "Success! You've been checked in!" }
     else
