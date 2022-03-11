6ee678a8fc058a17afe682accb7d35b41a80e389
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-10 17:09:52 -0800

    update routing and login

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -1,34 +1,26 @@
 class CheckinController < ApplicationController
-
-  # def new
-  #   @checkin = Checkin.new
-  #   # Get the current user to be used to autofill form info
-  #   # @user = User.find_by_id(session[:current_user_id])
-  #   # If cannot find user, then redirect to login
-  #   # otherwise redirect to create
-  #   # for testing
-  #   @user = User.all.first
-  #   redirect_to :action => "create"
-  # end
+  before_action :require_login
 
   def create
     flash.clear
     Time.zone = 'Pacific Time (US & Canada)'
-    # Get the current user to be used to autofill form info
-    # @user = Student.find_by_id(session[:current_user_id])
-    # If cannot find user, then redirect to login
-    # otherwise redirect to create
-    # for testing
-    @user = Student.create(first_name: "Jane", last_name: "Doe", sid: 123456789, email: "janedoe@email.com", password: "password", is_student: true, is_admin: false, is_staff: false)
+    @user = Student.find_by_id(session[:current_user_id])
     @checkin = Checkin.new
-    @checkin.time = DateTime.now
-    @checkin.student_id = @user.id
+    @checkin.update(time: DateTime.now, student_id: @user.id)
     @checkin.student = @user
     # validate false for manual testing
     if @checkin.save(validate: false)
       redirect_to root_path, flash: { :success => "Success! You've been checked in!" }
     else
       redirect_to root_path, flash: { :error => "Something went wrong, please try again" }
+    end
+  end
+
+  private
+
+  def require_login
+    unless session.has_key? :current_user_id and Student.find_by_id(session[:current_user_id])
+      redirect_to root_path, flash: { :error => "Please sign-in before clicking the check-in button." }
     end
   end
 
