f1271e8864dc069d715488656d999c8d2eadb5fb
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-11 10:12:00 -0800

    remove last migration

--- db/migrate/20220310222144_drop_checkin_columns.rb
+++
@@ -1,7 +0,0 @@
-class DropCheckinColumns < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :checkins, :first_name
-    remove_column :checkins, :last_name
-    remove_column :checkins, :email
-  end
-end
