465f03df4f7acf6b544699e4d96cd4054a14553f
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-15 14:45:28 -0700

    finish page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+    before_action :authenticate
+
     def index
+
+    end
+
+    private
+
+    def authenticate
+        if session.has_key? :current_user_id and Student.find_by_id(session[:current_user_id])
+            @logged_out = false
+            student = Student.find_by_id(session[:current_user_id])
+            @name = student.first_name + " " + student.last_name
+        else
+            @logged_out = true
+        end
     end
 end
