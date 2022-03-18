00f13ae3ae4ca58db691e480cb8a3bd0bba8a840
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-18 16:00:22 -0700

    finish

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -1,16 +1,16 @@
 class CheckinController < ApplicationController
-  # before_action :require_login
+  before_action :require_login
 
   def new
     flash.clear
-    # @user = Student.find_by_id(session[:current_user_id])
+    @user = Student.find_by_id(session[:current_user_id])
     @checkin = Checkin.new
   end
 
   def create
     flash.clear
     Time.zone = 'Pacific Time (US & Canada)'
-    @checkin.update(time: DateTime.now, student_id: @user.id)
+    @checkin.update(time: DateTime.now, student_id: @user.id, reason: params[:checkin][:reason])
     if @checkin.save
       redirect_to root_path, flash: { :success => "Success! You've been checked in!" }
     else
