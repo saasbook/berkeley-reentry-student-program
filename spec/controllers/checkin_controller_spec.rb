7b190133c659131093ce0596fa29c1778a98a8e4
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-18 16:10:24 -0700

    add reason to test

--- spec/controllers/checkin_controller_spec.rb
+++ spec/controllers/checkin_controller_spec.rb
@@ -8,6 +8,7 @@ describe CheckinController do
     @expected_time = DateTime.parse("2022-03-08T12:00:00-08:00")
     allow(DateTime).to receive(:now).and_return @expected_time
     controller.session[:current_user_id] = @stu.id
+    params[:checkin][:reason] = "Studying"
   end
 
   describe "POST create" do
