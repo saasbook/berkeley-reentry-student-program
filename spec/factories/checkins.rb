d125cea9078309f19211688075bf35f744706a25
ryan-garay89 <josephrgaray89@gmail.com>
2022-03-26 12:27:07 -0700

    update factory & controller

--- spec/factories/checkins.rb
+++ spec/factories/checkins.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :checkin do
     Time.zone = 'Pacific Time (US & Canada)'
     time { Time.now }
+    reason { "Studying" }
     association :student, factory: :student
   end
 end
