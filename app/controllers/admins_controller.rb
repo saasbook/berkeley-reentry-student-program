d27e7743eb1563f78b096259350e70eaf522aceb
KLongLo <kinlonglo98065@gmail.com>
2022-03-27 21:55:51 -0700

    feat: deny access to admin dashboard from non admin users

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,18 @@
 class AdminsController < ApplicationController
+  before_action :check_permission
+
   def index
   end
 
   def view_checkin_records
+  end
+
+  private
+  
+  def check_permission
+    admin = Admin.find_by_id(session[:current_user_id])
+    if !admin || !admin.is_admin
+      redirect_to root_path, flash: { :error => "You don't have the permission to do that!" }
+    end
   end
 end
