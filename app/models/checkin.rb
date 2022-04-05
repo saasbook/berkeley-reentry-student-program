e83370f6a96acee1d5d467e3ecc62122b7311e0a
KLongLo <kinlonglo98065@gmail.com>
2022-04-04 23:09:25 -0700

    feat: add method to Checkin model
    can get the n-th 20 checkin records(sorted by time in descending order)

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,4 +1,11 @@
 class Checkin < ApplicationRecord
   validates :time, :student_id, :reason, :presence => true
   belongs_to :student
+
+  # get the 20 checkin records start from index 20*n (sorted by time)
+  def self.get_20_checkin_records n
+    from_index = n >= 0 ? 20 * n : 0
+    records = Checkin.all.order(time: :desc)[from_index, 20] || []
+    records
+  end
 end
