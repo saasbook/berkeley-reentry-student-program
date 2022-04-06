307b42b2eed3d52b9509c577aa17ec70a0c74ef1
ryan-garay89 <josephrgaray89@gmail.com>
2022-04-06 16:14:56 -0700

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_214012) do
+ActiveRecord::Schema.define(version: 2022_04_06_231323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2022_03_31_214012) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.boolean "is_student"
     t.boolean "is_admin"
     t.boolean "is_staff"
