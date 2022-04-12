8a0c20ee0725303e5216243b17a98881d55f0ffb
ryan-garay89 <josephrgaray89@gmail.com>
2022-04-11 19:10:53 -0700

    conventional update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,31 @@
 class UsersController < ApplicationController
+  before_action :require_login
+
   def update
     flash.clear
     @user = User.find_by(id: session[:current_user_id])
-    if @user.nil?
-      redirect_to login_path, flash: { :error => "Please log-in!" }
-      return
-    end
-    first_name = params[:user][:first_name]
-    last_name = params[:user][:last_name]
-    email = params[:user][:email]
     sid = params[:user][:sid]
     if sid.blank? or sid.length != 10
       redirect_to login_confirm_path, flash: { :error => "Invalid Student ID Number" }
       return
     end
-    if @user.update(first_name: first_name, last_name: last_name, email: email, sid: sid)
+    if @user.update(user_params)
       redirect_to root_path, flash: { :success => "Success! You've been logged-in!" }
     else
       redirect_to login_confirm_path, flash: { :error => "Something went wrong, please try again." }
     end
   end
+
+  private
+
+  def require_login
+    unless session.has_key? :current_user_id and Student.find_by_id(session[:current_user_id])
+      redirect_to login_path, :method => :get
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :sid)
+  end
+
 end
