23b296eca7d8f3af256d1642fbac7cbaaf64b081
ryan-garay89 <josephrgaray89@gmail.com>
2022-04-12 11:52:06 -0700

    finsh testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :require_login
+  before_action :require_login
 
   def profile_new
     flash.clear
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+include FactoryBot::Syntax::Methods
+
+describe UsersController do
+  describe "update profile" do
+    before do
+      @student = create :student
+      controller.session[:current_user_id] = @student.id
+    end
+
+    it "should update the user record" do
+      patch :profile_update, params: {profile: {major: "computer science", grad_year: DateTime.new(2022), pronouns: "He/Him/His", identities: "something"} }
+      @student.reload
+      expect(@student.major).to eq "computer science"
+      expect(@student.grad_year).to eq DateTime.new(2022, 01, 01)
+      expect(@student.pronouns).to eq "He/Him/His"
+      expect(@student.identities).to eq "something"
+    end
+
+    it "should not update user on skip" do
+      @student.update(major: "", grad_year: nil, pronouns: "", identities: "")
+      @student.reload
+      patch :profile_update, params: {profile: {major: "computer science", grad_year: DateTime.new(2022), pronouns: "He/Him/His", identities: "something", skip: "Skip"} }
+      expect(@student.major).to eq ""
+      expect(@student.grad_year).to eq nil
+      expect(@student.pronouns).to eq ""
+      expect(@student.identities).to eq ""
+    end
+
+    it "should redirect on logged-out user" do
+      controller.session[:current_user_id] = ""
+      patch :profile_update, params: {profile: {major: "computer science", grad_year: DateTime.new(2022), pronouns: "He/Him/His", identities: "something", skip: "Skip"} }
+      expect(response).to redirect_to login_path
+    end
+  end
+end
