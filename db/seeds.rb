bce9374c70738a3ed7428ac978b8907bbd3c144f
KLongLo <kinlonglo98065@gmail.com>
2022-04-17 00:39:47 -0700

    chore: add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-for x in 1..10 do
-  FactoryBot.create(:student)
+
+r = Random.new
+students = []
+staffs = []
+admin = FactoryBot.create(:admin)
+
+10.times do
+  students.push FactoryBot.create(:student)
   # create 10 students and save them
 end
+
+3.times do
+  staffs.push FactoryBot.create(:staff)
+end
+
+checkin_reasons = ['Peer Support', 'Counseling Appointment', 'Studying', 'OWLs Meeting', 'Other']
+40.times do
+  FactoryBot.create(:checkin, reason: checkin_reasons.sample, student: students.sample, time: Time.current + r.rand(-240.hours..0.hours))
+end
+
+20.times do
+  FactoryBot.create(:appointment, staff: staffs.sample, student: students.sample, time: Time.current + r.rand(-240.hours..240.hours))
+end
+
+10.times do
+  FactoryBot.create(:appointment, staff: staffs.sample, student: nil, time: Time.current + r.rand(1.hours..240.hours))
+end
+
