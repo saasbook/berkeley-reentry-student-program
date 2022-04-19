ba52d041d8bebecb0beb33f9bc1393c7013711e4
Michelle Kroll <michellekroll@berkeley.edu>
2022-04-18 22:39:33 -0700

    fixed requested changes

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,13 @@
 class AppointmentsController < ApplicationController
+    before_action :require_login
+
     def advisors
     end
+
+    def require_login
+        unless session.has_key? :current_user_id and Student.find_by_id(session[:current_user_id])
+          redirect_to login_path, :method => :get
+        end
+      end
+      
 end
