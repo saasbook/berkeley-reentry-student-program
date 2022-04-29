87e8f0887dbf5472c1253f34ccf21420d1ca64fa
ryan-garay89 <josephrgaray89@gmail.com>
2022-04-29 15:34:57 -0700

    fix

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,6 +8,6 @@ class AppointmentsController < ApplicationController
         unless session.has_key? :current_user_id and Student.find_by_id(session[:current_user_id])
           redirect_to login_path, :method => :get
         end
-      end
+    end
       
 end
