29a461067265e247f806c2051af093d2a55a4c56
ryan-garay89 <josephrgaray89@gmail.com>
2022-05-01 15:30:45 -0700

    remove commented routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   root to: 'pages#index'
-  # route GET /login to login controller and #index action
-  get 'login/confirm', to: 'login#confirm'
   # route GET /check-in to login controller and #index action
   get "checkin", to: "checkin#new"
   post "checkin", to: "checkin#create"
@@ -25,4 +22,5 @@ Rails.application.routes.draw do
   get 'auth/google_oauth2/callback', to: 'sessions#google_auth', as: 'google_login'
   get 'auth/failure', to: redirect('/')
   get 'logout', to: 'sessions#google_auth_logout'
+  get 'login/confirm', to: 'login#confirm'
 end
