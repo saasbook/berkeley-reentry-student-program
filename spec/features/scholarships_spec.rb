442f6a4c202ab3f34a79558f3fc32b19f65ba5fd
MaxYang24 <yangm24@berkeley.edu>
2023-04-05 12:40:13 -0700

    incomplete rspec for scholar

--- spec/features/scholarships_spec.rb
+++ spec/features/scholarships_spec.rb
@@ -2,6 +2,21 @@
 
 require 'rails_helper'
 
+ADMIN_CREDENTIALS = {
+  "provider" => 'google_oauth2',
+  "uid" =>      '1000000000',
+  "info" =>     {
+    "name" =>       'Google Admin Developer',
+    "email" =>      'google_admin@berkeley.edu',
+    "first_name" => 'Google',
+    "last_name" =>  'Admin Developer'
+  },
+  "credentials" => {
+    "token" => "credentials_token_1234567",
+    "refresh_token" => "credentials_refresh_token_45678"
+  }
+}
+
 RSpec.feature "Scholarships", type: :feature do
 
   scenario "user can view scholarships index page" do
