ad3cccd551915c0275842176997260d24d07201d
Ethan Xu <xuqibao0816@gmail.com>
2023-04-22 11:08:34 -0700

    back top js not working

--- app/javascript/backtotop.js
+++ app/javascript/backtotop.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+    $('#back-to-top').hide();
+
+    // show or hide button depending on scroll position
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 20) {
+        if ($(this).scrollTop() > 10) {
             $('#back-to-top').fadeIn();
         } else {
             $('#back-to-top').fadeOut();
@@ -8,9 +11,7 @@ $(document).ready(function() {
     });
 
     $('#back-to-top').click(function() {
-        $('body, html').animate({
-            scrollTop: 0
-        }, 400);
+        $('body, html').animate({scrollTop: 0}, 400);
         return false;
     })
 })
